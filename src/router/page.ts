52dedf50acf89577e59b5923405b5aedbef0c672
Jongkeun <jongkeun.kr@gmail.com>
2020-02-27 11:58:55 +0900

    add comment to page.ts

--- src/router/page.ts
+++ src/router/page.ts
@@ -1,14 +1,14 @@
 import * as express from "express";
 let router = express.Router();
-
+// localhost/page/about
 router.get("/about", (req: express.Request, res: express.Response) => {
   res.send("page default!");
 });
-
+// localhost/page/a
 router.get("/a", (req: express.Request, res: express.Response) => {
   res.send("page a!");
 });
-
+// localhost/page/b
 router.get("/b", function(req: express.Request, res: express.Response) {
   res.send("page b!");
 });
