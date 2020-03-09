fdbec853b99e854d86e553ab5a7056ed5b6e78df
Jongkeun <dldkfod@naver.com>
2020-03-09 20:26:40 +0900

    add cors setting about all requested url

--- src/web.ts
+++ src/web.ts
@@ -1,12 +1,13 @@
 import * as express from "express";
 import App from "./app";
 import router from "./router/main";
+import * as cors from "cors";
 
 const app: express.Application = new App().application;
 const PORT: number = parseInt(process.env.PORT) || 4000;
 // set router at ./router/main
+app.use(cors());
 router(app);
-
 // start point
 app.listen(PORT, () => {
   console.log(`✅  Server is listening on port ${PORT}!`);
